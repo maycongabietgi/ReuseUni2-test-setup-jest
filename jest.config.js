4a7fa1138ad79cb4716985f4fadb612a327796df
MartinDawn <phamgiatri2k4@gmail.com>
2025-12-09 22:37:39 +0700

    Add coverage workflow

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['./jest.setup.js'],
+
   transformIgnorePatterns: [
     'node_modules/(?!(' +
       '@react-native|' +
@@ -11,7 +12,25 @@ module.exports = {
       'react-native-safe-area-context' +
     ')/)',
   ],
+
   moduleNameMapper: {
     '\\.(png|jpg|jpeg|gif)$': '<rootDir>/__mocks__/fileMock.js',
   },
+
+  // 👉 BẮT BUỘC: Thu coverage
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+
+  // 👉 BẮT BUỘC: HTML report để làm artifact cho thầy
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./test-report",   // folder artifact
+        filename: "index.html",        // file thầy yêu cầu
+        expand: true
+      }
+    ]
+  ]
 };
